8ea7e343343f1bb91c197fafd3b8ae18c451284a
Gaffer Fitch <gfitch@digitalocean.com>
2015-12-30 08:12:50 -0500

    Not regenerating SSL, migrating db

--- vagrant/demo-do/provision.sh
+++ vagrant/demo-do/provision.sh
@@ -26,6 +26,8 @@ sudo pip install djangorestframework==3.1
 
 if sudo mysql -u root -e "show databases" | grep -w hundoword_django; then
     echo "Database hundoword_django exists"
+    python /home/vagrant/src/server/hundoword_django/manage.py makemigrations learning --noinput
+    python /home/vagrant/src/server/hundoword_django/manage.py migrate --noinput
 else
     /vagrant/db.sh
 fi
@@ -34,7 +36,7 @@ sudo apt-get install -y apache2 libapache2-mod-wsgi
 sudo rm -f /etc/apache2/sites-enabled/000-default.conf
 sudo cp -f /vagrant/ports.conf /etc/apache2/ports.conf
 sudo cp /vagrant/api.conf /etc/apache2/sites-available/api.conf
-sudo ln -sf /etc/apache2/sites-available/api.conf /etc/apache2/sites-enabled/api.conf 
+sudo ln -sf /etc/apache2/sites-available/api.conf /etc/apache2/sites-enabled/api.conf
 sed "s/<<<secret_key>>>/'`pwgen -N 7 -s | tr '\n' ' ' | sed -e 's/ //g'`'/" /vagrant/settings.py > /home/vagrant/src/server/hundoword_django/hundoword_django/settings.py
 sudo chown -R www-data:www-data /home/vagrant/src/server/hundoword_django/
 sudo service apache2 restart
@@ -46,6 +48,10 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo cp /vagrant/www.conf /etc/nginx/sites-available/www.conf
 sed "s/<<<ip>>>/`ifconfig eth0 | grep -oP 'inet addr:[0-9\\.]+' | grep -oP '[0-9\\.]+'`/g" /vagrant/www.conf > /etc/nginx/sites-available/www.conf
 sudo ln -sf /etc/nginx/sites-available/www.conf /etc/nginx/sites-enabled/www.conf
-sudo mkdir -p /etc/nginx/ssl/
-sudo openssl req -new -newkey rsa:4096 -days 365 -nodes -x509  -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=US/ST=Learning/L=Present/O=Hundoword/CN=demo.hundoword.com"
+if [ -d /etc/nginx/ssl/ ]; then 
+    echo "Nginx ssl exists"
+else
+    sudo mkdir -p /etc/nginx/ssl/
+    sudo openssl req -new -newkey rsa:4096 -days 365 -nodes -x509  -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=US/ST=Learning/L=Present/O=Hundoword/CN=demo.hundoword.com"
+fi
 sudo service nginx restart
