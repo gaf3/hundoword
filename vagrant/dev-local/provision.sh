3b1d19f5609ea2885ef857205413b62ed578f79a
Gaffer Fitch <gfitch@digitalocean.com>
2016-01-04 19:33:51 -0500

    Moved provision for db

--- vagrant/dev-local/provision.sh
+++ vagrant/dev-local/provision.sh
@@ -26,6 +26,7 @@ sudo pip install requests==2.5.3
 sudo pip install Django==1.8.4
 sudo pip install jsonfield
 sudo pip install djangorestframework==3.1
+sudo -u vagrant cp /vagrant/settings.py /home/vagrant/src/server/hundoword_django/hundoword_django/settings.py
 
 if sudo mysql -u root -e "show databases" | grep -w hundoword_django; then
     echo "Database hundoword_django exists"
@@ -40,7 +41,6 @@ sudo rm -f /etc/apache2/sites-enabled/000-default
 sudo cp -f /vagrant/ports.conf /etc/apache2/ports.conf
 sudo cp /vagrant/api.hundoword.com /etc/apache2/sites-available/api.hundoword.com
 sudo ln -s /etc/apache2/sites-available/api.hundoword.com /etc/apache2/sites-enabled/api.hundoword.com
-sudo -u vagrant cp /vagrant/settings.py /home/vagrant/src/server/hundoword_django/hundoword_django/settings.py
 sudo service apache2 restart
 
 # WWW
